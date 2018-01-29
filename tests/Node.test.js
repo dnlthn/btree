2bc8fa256681edd42bb8767efcc12f9385a64d8c
Daniel Thompson <dthompson844@gmail.com>
2018-01-28 21:25:38 -0600

    Added Node class tests

---
+++ tests/Node.test.js
@@ -0,0 +1,43 @@
+import Node from "../src/Node";
+
+test("Node instantiates a Node object", () => {
+  expect(new Node({ data: 1 }) instanceof Node).toBe(true);
+});
+
+test("Constructor sets the correct value", () => {
+  const node_one = new Node({ data: 1 });
+  expect(node_one.data).toBe(1);
+});
+
+test("Constructor inserts left correctly", () => {
+  const node_one = new Node({ data: 1 });
+  const root_node = new Node({ data: 2, left: node_one });
+
+  const left_node = root_node.left;
+  expect(left_node.data).toBe(1);
+});
+
+test("Constructor inserts right correctly", () => {
+  const node_one = new Node({ data: 1 });
+  const root_node = new Node({ data: 2, right: node_one });
+
+  const right_node = root_node.right;
+  expect(right_node.data).toBe(1);
+});
+
+test("Returns correct node when deeply nested", () => {
+  const four = new Node({ data: 4 });
+  const two = new Node({ data: 2, right: four });
+  const root_node = new Node({ data: 6, left: two });
+
+  const result = root_node.left.right;
+  expect(result.data).toBe(4);
+});
+
+test("Returns null for a leaf", () => {
+  const two = new Node({ data: 2 });
+  const root_node = new Node({ data: 6, left: two });
+
+  const result = root_node.right;
+  expect(result).toBe(null);
+});
