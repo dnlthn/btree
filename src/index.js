39602795ad188418cac2e408aa07eb6ea7b8a902
Daniel Thompson <dthompson844@gmail.com>
2018-01-29 04:06:48 -0600

    Refactored ParentFinder to remove nested if's

--- src/index.js
+++ src/index.js
@@ -2,26 +2,11 @@ const ParentFinder = (tree, node_one, node_two) => {
   const parents_one = tree.get_parents(node_one.data);
   const parents_two = tree.get_parents(node_two.data);
 
-  const one_has_more_parents = parents_one.length > parents_two.length;
-  if (one_has_more_parents) {
-    if (
-      parents_one[parents_two.length - 1] ===
-      parents_two[parents_two.length - 1]
-    ) {
-      return parents_one[parents_two.length - 1];
-    } else {
-      return parents_one[0];
-    }
-  } else {
-    if (
-      parents_one[parents_one.length - 1] ===
-      parents_two[parents_one.length - 1]
-    ) {
-      return parents_one[parents_one.length - 1];
-    } else {
-      return parents_one[0];
-    }
-  }
+  const min_parents = Math.min(parents_one.length, parents_two.length) - 1;
+
+  if (parents_one[min_parents] === parents_two[min_parents])
+    return parents_one[min_parents];
+  else return parents_one[0];
 };
 
 export default ParentFinder;
