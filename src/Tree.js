efa2113b5740d04cf2e3e62ba3da6c6fd0d8bbda
Daniel Thompson <dthompson844@gmail.com>
2018-01-29 02:21:46 -0600

    Moved recursive insert and refactored contains/find to non-accessible methods

--- src/Tree.js
+++ src/Tree.js
@@ -1,4 +1,4 @@
-class Tree {
+export default class Tree {
   constructor(args) {
     const { root = null, comparator = false } = args || {};
     this.root = root;
@@ -7,37 +7,21 @@ class Tree {
 
   insert(node) {
     if (this.root === null) this.root = node;
-    else this.recursive_insert(node, this.root);
-  }
-
-  recursive_insert(node, current) {
-    const less_than_current = this.compare(node, current);
-
-    if (less_than_current) {
-      if (current.left) this.recursive_insert(node, current.left);
-      else current.left = node;
-    } else {
-      if (current.right) this.recursive_insert(node, current.right);
-      else current.right = node;
-    }
+    else recursive_insert(node, this.root, { comparator: this.compare });
   }
 
   contains(value, current = this.root) {
-    if (!current) return false;
-    if (value === current.data) return true;
-
-    const less_than_current = this.compare({ data: value }, current);
-    if (less_than_current) return this.contains(value, current.left);
-    else return this.contains(value, current.right);
+    return search_helper(value, current, {
+      return_node: false,
+      comparator: this.compare
+    });
   }
 
   find(value, current = this.root) {
-    if (!current) return null;
-    if (value === current.data) return current;
-
-    const less_than_current = this.compare({ data: value }, current);
-    if (less_than_current) return this.find(value, current.left);
-    else return this.find(value, current.right);
+    return search_helper(value, current, {
+      return_node: true,
+      comparator: this.compare
+    });
   }
 
   get_parents(value, current = this.root, parents = []) {
@@ -50,4 +34,32 @@ class Tree {
     else return this.get_parents(value, current.right, [...parents, current]);
   }
 }
-export default Tree;
+
+function recursive_insert(node, current, { comparator }) {
+  const less_than_current = comparator(node, current);
+
+  if (less_than_current) {
+    if (current.left) recursive_insert(node, current.left, { comparator });
+    else current.left = node;
+  } else {
+    if (current.right) recursive_insert(node, current.right, { comparator });
+    else current.right = node;
+  }
+}
+
+function search_helper(value, current, { return_node = false, comparator }) {
+  if (!current) return return_node ? null : false;
+  if (value === current.data) return return_node ? current : true;
+
+  const less_than_current = comparator({ data: value }, current);
+  if (less_than_current)
+    return search_helper(value, current.left, {
+      return_node,
+      comparator
+    });
+  else
+    return search_helper(value, current.right, {
+      return_node,
+      comparator
+    });
+}
